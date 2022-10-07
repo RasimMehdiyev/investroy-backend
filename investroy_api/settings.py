9cfed91c4dc9c700cec90cdf4a620301a18c9f8f
Rasim <rasimmehdiyev@edu.bme.hu>
2022-10-07 21:46:27 +0200

    static

--- investroy_api/settings.py
+++ investroy_api/settings.py
@@ -114,9 +114,9 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
